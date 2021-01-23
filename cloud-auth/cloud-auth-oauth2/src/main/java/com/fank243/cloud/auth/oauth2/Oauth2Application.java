353713ace3a660586096498efde7fa6d83e976ab
FanWeiJie <fank243@163.com>
2021-01-23 22:48:50 +0800

    重构代码

--- cloud-auth/cloud-auth-oauth2/src/main/java/com/fank243/cloud/auth/oauth2/Oauth2Application.java
+++ cloud-auth/cloud-auth-oauth2/src/main/java/com/fank243/cloud/auth/oauth2/Oauth2Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
